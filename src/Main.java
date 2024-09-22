c9b529d4a379bf7923e9290fc5bd921784dc5486
thiagosnx <thiagosantanacq@gmail.com>
2024-09-22 19:06:56 -0300

    feat: funci servico

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import dominio.Funcionario;
-import dominio.cargos.Cargo;
 import dominio.cargos.Gerente;
 import dominio.cargos.Secretario;
 import dominio.cargos.Vendedor;
-import servicos.ServicoCalculaAnos;
-import servicos.ServicoCalculaSalario;
+import servicos.ServicoFuncionario;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         List<Funcionario> funcionarios = new ArrayList<>();
-        ServicoCalculaSalario servicoCalculaSalario = new ServicoCalculaSalario();
+        ServicoFuncionario servicoFuncionario = new ServicoFuncionario();
+        servicoFuncionario.criaFuncionarios();
         funcionarios.add(
                 new Funcionario(1
                 , "Jorge Carvalho"
@@ -29,13 +29,13 @@ public class Main {
         funcionarios.add(
                 new Funcionario(1
                         , "Ana Silva"
-                        , new Vendedor(1000.00)
+                        , new Vendedor(26250.00)
                         , "2021-12-01")
         );
         funcionarios.add(
                 new Funcionario(1
                         , "João Mendes"
-                        , new Vendedor(1000.00)
+                        , new Vendedor(28500.00)
                         , "2021-12-01")
         );
         funcionarios.add(
@@ -50,17 +50,24 @@ public class Main {
                         , new Gerente()
                         , "2014-03-01")
         );
-        for (Funcionario funcionario : funcionarios){
-            System.out.println(
-                    "Nome: " + funcionario.getNome() + "\n" +
-                    "Cargo: " + funcionario
-                            .getCargo()
-                            .getClass()
-                            .getSimpleName() + "\n" +
-                    "Anos de Casa: " + funcionario.getAnosServico() + "\n" +
-                    "Salario Total: " + servicoCalculaSalario.calculaSalario(funcionario.getCargo()) + "\n"
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+        while(opcao != 6){
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Ver lista dos funcionários e seus respectivos salários e benefícios.");
+            System.out.println("2. Ver lista dos funcionários e seus salários sem benefícios.");
+            System.out.println("3. Ver lista somente dos funcionários que recebem benefícios");
+            System.out.println("4. Ver funcionário que recebeu maior salário no mês.");
+            System.out.println("5. Ver funcionário que recebeu a maior quantia em benefícios no mês.");
+            System.out.println("6. Ver funcionário que mais vendeu no mês.");
+            opcao = scanner.nextInt();
 
-            );
+            switch (opcao){
+                case 1:
+                    servicoFuncionario.retornaFuncionarioCompleto();
+                    break;
+            }
         }
+
     }
 }
---
+++ src/servicos/ServicoFuncionario.java
@@ -0,0 +1,69 @@
+package servicos;
+
+import dominio.Funcionario;
+import dominio.cargos.Gerente;
+import dominio.cargos.Secretario;
+import dominio.cargos.Vendedor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoFuncionario {
+    List<Funcionario> funcionarios = new ArrayList<>();
+    ServicoCalculaSalario servicoCalculaSalario = new ServicoCalculaSalario();
+    public void criaFuncionarios(){
+
+        funcionarios.add(
+                new Funcionario(1
+                        , "Jorge Carvalho"
+                        , new Secretario()
+                        , "2018-01-01")
+        );
+        funcionarios.add(
+                new Funcionario(2
+                        , "Maria Souza"
+                        , new Secretario()
+                        , "2015-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Ana Silva"
+                        , new Vendedor(26250.00)
+                        , "2021-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "João Mendes"
+                        , new Vendedor(28500.00)
+                        , "2021-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Juliana Alves"
+                        , new Gerente()
+                        , "2017-07-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Bento Albino"
+                        , new Gerente()
+                        , "2014-03-01")
+        );
+    }
+    public void retornaFuncionarioCompleto(){
+        for (Funcionario funcionario : funcionarios){
+            System.out.println(
+                    "Nome: " + funcionario.getNome() + "\n" +
+                            "Cargo: " + funcionario
+                            .getCargo()
+                            .getClass()
+                            .getSimpleName() + "\n" +
+                            "Anos de Casa: " + funcionario.getAnosServico() + "\n" +
+                            "Salario Total: " + servicoCalculaSalario.calculaSalario(funcionario.getCargo()) + "\n"
+
+            );
+        }
+    }
+
+
+}
