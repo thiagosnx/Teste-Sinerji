3850d8d2e0a6880883166c2ca27ad6503be826fc
thiagosnx <thiagosantanacq@gmail.com>
2024-09-22 18:34:01 -0300

    feat: add funcionarios

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,66 @@
 import dominio.Funcionario;
 import dominio.cargos.Cargo;
+import dominio.cargos.Gerente;
+import dominio.cargos.Secretario;
 import dominio.cargos.Vendedor;
 import servicos.ServicoCalculaAnos;
 import servicos.ServicoCalculaSalario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        ServicoCalculaAnos calc = new ServicoCalculaAnos();
+        List<Funcionario> funcionarios = new ArrayList<>();
         ServicoCalculaSalario servicoCalculaSalario = new ServicoCalculaSalario();
-        Funcionario funcionario = new Funcionario(
-                1
-                , "Teste"
-                , new Vendedor(5000.00)
-                , "2018-01-01");
-        funcionario.exibeFuncionario();
+        funcionarios.add(
+                new Funcionario(1
+                , "Jorge Carvalho"
+                , new Secretario()
+                , "2018-01-01")
+        );
+        funcionarios.add(
+                new Funcionario(2
+                        , "Maria Souza"
+                        , new Secretario()
+                        , "2015-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Ana Silva"
+                        , new Vendedor(1000.00)
+                        , "2021-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "João Mendes"
+                        , new Vendedor(1000.00)
+                        , "2021-12-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Juliana Alves"
+                        , new Gerente()
+                        , "2017-07-01")
+        );
+        funcionarios.add(
+                new Funcionario(1
+                        , "Bento Albino"
+                        , new Gerente()
+                        , "2014-03-01")
+        );
+        for (Funcionario funcionario : funcionarios){
+            System.out.println(
+                    "Nome: " + funcionario.getNome() + "\n" +
+                    "Cargo: " + funcionario
+                            .getCargo()
+                            .getClass()
+                            .getSimpleName() + "\n" +
+                    "Anos de Casa: " + funcionario.getAnosServico() + "\n" +
+                    "Salario Total: " + servicoCalculaSalario.calculaSalario(funcionario.getCargo()) + "\n"
+
+            );
+        }
     }
 }
